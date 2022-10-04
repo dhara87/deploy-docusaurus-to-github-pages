02fd49ca7efa5270d25afa43816d66041fa3f89e
Dhara Patel <dhara@atcults.com>
2022-10-04 14:09:11 +0530

    change in config

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -38,14 +38,14 @@ const config = {
           // Please change this to your repo.
           // Remove this to remove the "edit this page" links.
           editUrl:
-            'https://github.com/dhara87/deploy-docusaurus-to-github-pages/tree/main/docs/',
+            'https://github.com/dhara87/deploy-docusaurus-to-github-pages/tree/main/',
         },
         blog: {
           showReadingTime: true,
           // Please change this to your repo.
           // Remove this to remove the "edit this page" links.
           editUrl:
-            'https://github.com/facebook/dhara87/deploy-docusaurus-to-github-pages/tree/main/blog/',
+            'https://github.com/dhara87/deploy-docusaurus-to-github-pages/tree/main/',
         },
         theme: {
           customCss: require.resolve('./src/css/custom.css'),
